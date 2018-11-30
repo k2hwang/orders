7a3e249725a554aa5bf0fc5ddc790b0a62ae3046
Hwang <kthwang@f40f240f98ea.ant.amazon.com>
2018-11-30 20:55:32 +0900

    buildspec updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,7 +25,7 @@ CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 $DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -DskipTests package
 
-cp -r $CODE_DIR/docker $CODE_DIR/target/docker/
+cp -r $CODE_DIR/docker/ $CODE_DIR/target/docker/
 cp -r $CODE_DIR/target/*.jar $CODE_DIR/target/docker/${IMAGE}
 
 REPO=${GROUP}/${IMAGE}
