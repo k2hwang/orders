52d6e13c15d34f898528594fc325dc7cf57090ba
Hwang <kthwang@f40f240f98ea.ant.amazon.com>
2018-12-01 00:02:48 +0900

    debug log enabled @ application.properties

--- src/main/java/works/weave/socks/orders/config/DynamoDBConfiguration.java
+++ src/main/java/works/weave/socks/orders/config/DynamoDBConfiguration.java
@@ -8,28 +8,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 
 public class DynamoDBConfiguration {
-    @Value(“${amazon.dynamodb.endpoint}”)
-    private String amazonDynamoDBEndpoint;@Value(“${amazon.aws.accesskey}”)
-    private String amazonAWSAccessKey;@Value(“${amazon.aws.secretkey}”)
-    private String amazonAWSSecretKey;
+    // @Value(“${amazon.dynamodb.endpoint}”)
+    // private String amazonDynamoDBEndpoint;@Value(“${amazon.aws.accesskey}”)
+    // private String amazonAWSAccessKey;@Value(“${amazon.aws.secretkey}”)
+    // private String amazonAWSSecretKey;
 
-    @Bean
-    public AmazonDynamoDB amazonDynamoDB() {
-        AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(amazonAWSCredentials());
-        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
-            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
-        }
-        return amazonDynamoDB;
-    }
+    // @Bean
+    // public AmazonDynamoDB amazonDynamoDB() {
+    //     AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(amazonAWSCredentials());
+    //     if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
+    //         amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+    //     }
+    //     return amazonDynamoDB;
+    // }
 
-    @Bean
-    public AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
-    }
+    // @Bean
+    // public AWSCredentials amazonAWSCredentials() {
+    //     return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+    // }
 
-    public AmazonDynamoDBClient getClient() {
-        BasicAWSCredentials credentials = new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
-        AmazonDynamoDBClient client = new AmazonDynamoDBClient(credentials).withRegion(Regions.AP_SOUTHEAST_1);
-        return client;
-    }
+    // public AmazonDynamoDBClient getClient() {
+    //     BasicAWSCredentials credentials = new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+    //     AmazonDynamoDBClient client = new AmazonDynamoDBClient(credentials).withRegion(Regions.AP_SOUTHEAST_1);
+    //     return client;
+    // }
 }
